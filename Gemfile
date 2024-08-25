d43f9e1ec92acbfb4eb944bee8bdad93b0feeb5a
cjw <choi.jae100@gmail.com>
2024-08-25 11:06:47 +0000

    Styling attempt

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source "https://rubygems.org"
 
 gem "dotenv-rails", groups: [ :development, :test ]
 
+gem 'will_paginate', '~> 4.0'
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.2.0"
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
--- app/controllers/job_listings_controller.rb
+++ app/controllers/job_listings_controller.rb
@@ -1,5 +1,9 @@
 class JobListingsController < ApplicationController
-  def index
-      @job_listings = JobListing.all
-  end
+#   def index
+#       @job_listings = JobListing.all
+#   end
+
+    def index
+        @job_listings = JobListing.order(created_at: :desc).paginate(page: params[:page], per_page: 10)
+    end
 end
